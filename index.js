e253c9c9ffa214266be0d1e80597f0963e392846
Paulo Tanaka <ptanaka@linkedin.com>
2016-01-09 23:23:37 -0800

    Adding repl

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // This is the entrypoint for our product
 var Account = require('./lib/Account');
+var repl = require('repl')
 
 var johnAccount = new Account();
 
+console.log('John\'s balance is:');
 console.log(johnAccount.balance);
 
+
+repl = repl.start('Bank > ');
+repl.context.Account = Account;
